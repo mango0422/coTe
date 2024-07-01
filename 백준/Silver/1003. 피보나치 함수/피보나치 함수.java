2042a6fd90df888994a5c6d7698e97a28e74220e
mango0422 <83891638+mango0422@users.noreply.github.com>
2024-07-01 12:04:56 +0900

    Update 피보나치 함수.java

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -4,7 +4,6 @@ public class Main {
     public static int[][] memo = new int[41][2];
 
     public static void main(String[] args) {
-        // 미리 피보나치 수열에서 0과 1이 호출되는 횟수를 계산
         fibonacciMemo();
 
         Scanner sc = new Scanner(System.in);
@@ -16,10 +15,10 @@ public class Main {
     }
 
     public static void fibonacciMemo() {
-        memo[0][0] = 1; // fibonacci(0)에서 0의 호출 횟수
-        memo[0][1] = 0; // fibonacci(0)에서 1의 호출 횟수
-        memo[1][0] = 0; // fibonacci(1)에서 0의 호출 횟수
-        memo[1][1] = 1; // fibonacci(1)에서 1의 호출 횟수
+        memo[0][0] = 1;
+        memo[0][1] = 0;
+        memo[1][0] = 0;
+        memo[1][1] = 1;
 
         for (int i = 2; i < 41; i++) {
             memo[i][0] = memo[i - 1][0] + memo[i - 2][0];
