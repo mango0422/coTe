24ccd4ab8091f7d77d17bf88c247052ece52d880
mango0422 <83891638+mango0422@users.noreply.github.com>
2025-03-11 21:09:09 +0900

    [Bronze II] Title: 수 정렬하기, Time: 112 ms, Memory: 14460 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,25 +1,24 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int[] arr = new int[n];
-		for(int i=0; i<n; i++) {
-			arr[i] = sc.nextInt();
-		}
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<n-i-1; j++) {
-				if(arr[j] > arr[j+1]) {
-					int tmp = arr[j];
-					arr[j] = arr[j+1];
-					arr[j+1] = tmp;
-				}
-			}
-		}
-		for(int i=0; i<n; i++) {
-			System.out.println(arr[i]);
-		}
-	}
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for (int num : arr) {
+            sb.append(num).append('\n');
+        }
+
+        System.out.print(sb);
+    }
 }
