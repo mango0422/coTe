09089c5fc375bf98dd9a8f65bd0671f24ae2bcff
mango0422 <83891638+mango0422@users.noreply.github.com>
2025-03-13 01:28:04 +0900

    [Bronze III] Title: 최소, 최대, Time: 488 ms, Memory: 110372 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,23 +1,36 @@
-import java.util.Scanner;
+import java.io.*;
 
-public class Main {
-	
-	public static void main(String[] args) {
-		
-		// https://www.acmicpc.net/problem/10818
-		
-		Scanner sc = new Scanner(System.in);
-		int min = 0, max = 0, n = sc.nextInt();
-		for(int i = 0; i<n; i++) {
-			int x = sc.nextInt();
-			if(i == 0) {
-				min = max = x;
-			}else if(i!=0 && x > max) {
-				max = x;
-			} else if(i!=0 && x < min) {
-				min = x;
-			}
-		}
-		System.out.println(min + " " + max);
-	}
+public class Main{
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = 0;
+        int min = 0;
+        int max = 0;
+        try{
+            n = Integer.parseInt(br.readLine());
+            int[] arr = new int[n];
+            String[] str = br.readLine().split(" ");
+            br.close();
+            for(int i=0; i<n; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+                if(i != 0) {
+                    if(arr[i] < min){
+                        min = arr[i];
+                    } else if(arr[i] > max){
+                        max = arr[i];
+                    }
+                } else{
+                    min = arr[i];
+                    max = arr[i];
+                }
+            }
+            bw.write(min + " " + max);
+            bw.flush();
+            bw.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
